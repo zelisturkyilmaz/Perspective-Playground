93f9a21d0a6101c1b923a8cf8df452a58985aced
Zelis Turkyilmaz <zelis.turkyilmaz1@gmail.com>
2023-10-22 14:40:28 +0300

    Add copy functionality to copy css with Clipboard API

--- app.js
+++ app.js
@@ -26,6 +26,13 @@ const vm = Vue.createApp({
             this.rotateX = 0
             this.rotateY = 0
             this.rotateZ = 0
+        },
+
+        async copy() {
+            let text = `transform:${this.box.transform};`
+            await navigator.clipboard.writeText(text)
+
+            alert('CSS Copied to Clipboard!')
         }
     }
 }).mount('#app')
